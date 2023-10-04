31e2ce5c572241b1075329e15df693c7aaa1ac3b
Nguyễn Hiếu <zhuongdae@gmail.com>
2023-10-04 22:39:34 +0700

    Add project files.

---
+++ Entities/T2207aApiContext.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace T2207A_API.Entities;
+
+public partial class T2207aApiContext : DbContext
+{
+    public T2207aApiContext()
+    {
+    }
+
+    public T2207aApiContext(DbContextOptions<T2207aApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cart> Carts { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-18SBTUC4;Initial Catalog=T2207A_API;Integrated Security=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cart>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("carts");
+
+            entity.Property(e => e.BuyQty)
+                .HasDefaultValueSql("((1))")
+                .HasColumnName("buy_qty");
+            entity.Property(e => e.ProductId).HasColumnName("product_id");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Product).WithMany()
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__carts__product_i__5812160E");
+
+            entity.HasOne(d => d.User).WithMany()
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__carts__user_id__571DF1D5");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__categori__3213E83F20E15C47");
+
+            entity.ToTable("categories");
+
+            entity.HasIndex(e => e.Name, "UQ__categori__72E12F1B3958EA4B").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__orders__3213E83F4834EAB8");
+
+            entity.ToTable("orders");
+
+            entity.HasIndex(e => e.InvoiceId, "UQ__orders__F58DFD48F4C97FE4").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.City)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("city");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.GrandTotal)
+                .HasColumnType("decimal(14, 2)")
+                .HasColumnName("grand_total");
+            entity.Property(e => e.InvoiceId)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("invoice_id");
+            entity.Property(e => e.PaymentMethod)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("payment_method");
+            entity.Property(e => e.ShippingAddress)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("shipping_address");
+            entity.Property(e => e.Status)
+                .HasDefaultValueSql("((1))")
+                .HasColumnName("status");
+            entity.Property(e => e.Tel)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("tel");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__orders__user_id__5DCAEF64");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3213E83F2AD1FC83");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CategoryId).HasColumnName("category_id");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(14, 2)")
+                .HasColumnName("price");
+            entity.Property(e => e.Qty).HasColumnName("qty");
+            entity.Property(e => e.Thumbnail)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("thumbnail");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__products__catego__52593CB8");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3213E83FB119B98C");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "UQ__users__AB6E61648B4223D7").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("address");
+            entity.Property(e => e.Avatar)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("avatar");
+            entity.Property(e => e.City)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("city");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Fullname)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("fullname");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("password");
+            entity.Property(e => e.Tel)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("tel");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Category/CreateCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace T2207A_API.Models.Category
+{
+    public class CreateCategory
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Nhap toi thieu 3 ki tu")]
+        [MaxLength(255, ErrorMessage = "Nhap toi thieu 255 ki tu")]
+        public string name { get; set; }
+    }
+}
